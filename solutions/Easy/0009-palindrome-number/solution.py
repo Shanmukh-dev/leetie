84fb279c0b0b4bfdbd2da81a84c18fddf3d08c66
Shanmukh-dev <shanmukhdev@users.noreply.github.com>
2026-08-18 06:25:38 +0000

    leetie: Add 9. Palindrome Number [Easy] (python3)

--- solutions/Easy/0009-palindrome-number/solution.py
+++ solutions/Easy/0009-palindrome-number/solution.py
@@ -3,8 +3,8 @@
 # Difficulty: Easy
 # Tags     : Math
 # Link     : https://leetcode.com/problems/palindrome-number/
-# Runtime  : 13 ms (beats 23%)
-# Memory   : 19196000 (beats 87%)
+# Runtime  : N/A (beats 0%)
+# Memory   : N/A (beats 0%)
 # Language : python3
 # Copyright: (c) 2026 Shanmukh-dev. All rights reserved.
 # Synced by: leetie
